c8c1e5fdf0accbbfcc299d93a2f7db342d30ebf2
Juha <juha.haipus@gmail.com>
2022-10-26 13:08:24 +0300

    paivitysta

--- osa1/anekdootit/src/App.js
+++ osa1/anekdootit/src/App.js
@@ -22,8 +22,11 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
   ]
+
+  const votes = new Uint8Array(7)
    
   const [selected, setSelected] = useState(0)
+  const [voted, setVote] = useState(0)
 
   const setToValue = newValue => {
     newValue = Randomizer(0, 6)
@@ -31,11 +34,26 @@ const App = () => {
     setSelected(newValue)
   }
 
+  const setToVote = newValue => {
+    if (votes.every(item => item === 0)) {
+      var copy = [...votes]
+      copy[selected] += 1
+      setVote(newValue)
+      console.log(copy)
+    }
+    else {
+    copy[selected] += 1
+    setVote(newValue)
+    console.log(copy)
+    }
+  }
+
   return (
     <div>
       <>{anecdotes[selected]}</>
       <p></p>
       <Button handleClick={() => setToValue(selected)} text="next anecdote" />
+      <Button handleClick={() => setToVote(voted)} text="vote" />
 
     </div>
   )
