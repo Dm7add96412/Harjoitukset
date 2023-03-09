3d29f285934f6a3d50744ddc58c721e18582c787
Dm7add9 <juha.haipus@gmail.com>
2023-03-09 22:59:55 +0200

    part4 exercises

--- osa3/Notebackend/mongo.js
+++ osa3/Notebackend/mongo.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
+/*
 if (process.argv.length<3) {
   console.log('give password as argument')
   process.exit(1)
 }
 
 const password = process.argv[2]
+*/
+mongoose.set('strictQuery', false)
 
-const url =
-  `mongodb+srv://fullstack:${password}@cluster0.feoxiak.mongodb.net/noteApp?retryWrites=true&w=majority`
+const url = process.env.TEST_MONGODB_URI
 
 mongoose.connect(url)
 
@@ -20,15 +23,17 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
+/*
 Note.find({ important: true }).then(result => {
   result.forEach(note => {
     console.log(note)
   })
   mongoose.connection.close()
 })
+*/
 
-/* const note = new Note({
-  content: 'Course material is good!',
+const note = new Note({
+  content: 'Something here!',
   date: new Date(),
   important: true,
 })
@@ -36,4 +41,4 @@ Note.find({ important: true }).then(result => {
 note.save().then(result => {
   console.log('note saved!')
   mongoose.connection.close()
-}) */
+})
---
+++ osa4/blogilista/tests/note_api.test.js
@@ -0,0 +1,22 @@
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.initialBlogs)
+})
+
+test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+afterAll(async () => {
+    await mongoose.connection.close()
+  })
