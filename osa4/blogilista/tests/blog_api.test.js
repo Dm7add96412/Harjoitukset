10694f535c00477649f7d60aa3e33453ff8c7305
Dm7add9 <juha.haipus@gmail.com>
2023-03-10 18:51:48 +0200

    4.9 exercise

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -11,10 +11,26 @@ beforeEach(async () => {
     await Blog.insertMany(helper.initialBlogs)
 })
 
+test('blogs are returned as json', async () => {
+    console.log('entered test')
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
 test('all blogs are returned', async () => {
     const response = await api.get('/api/blogs')
 
     expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+test('id in correct form', async () => {
+    const response = await api.get('/api/blogs')
+    let id = response.body.map(b => b.id)
+    id.forEach(id => {
+        expect(id).toBeDefined()
+    })
 })
 
 afterAll(async () => {
